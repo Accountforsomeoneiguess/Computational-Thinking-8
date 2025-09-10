0dc75360c911f168c59c5db81d9bbe74ed13ec3c
Accountforsomeoneiguess <leosusffalich@seattleacademy.org>
2025-09-10 19:11:13 +0000

    Second submission

--- project1.py
+++ project1.py
@@ -2,21 +2,99 @@ print("Hi! My name is Jim. What is your name?")
 name = input("")
 print(f"Hi {name} nice to meet you! What school do you go to?")
 school = input("")
-if school == "SAAS" or "Seattle Academy":
+if school == "SAAS":
     print("I go to SAAS too!")
 else:
     print(f"I don't go to {school}, I go to SAAS!")
 print(f"Do you like {school}?")
 like = input("")
 if like == "yes":
-    print("Cool! Me too!")
+    print("Cool! I like school too!")
 else:
     print("Ok! I like school!")
 print("What was your favorite class today?")
-variable4 = input("")
-if variable4 == "Instrumental Band" or "Band":
+class1 = input("")
+if class1 ==  "Band":
     print("Cool! Me too!")
 else: 
-    print("Cool! My favorite was Instrumental Band!")
+    print("Cool! My favorite was Band!")
+print("What class are you looking forward to?")
+class2 = input("")
+if school == "SAAS":
+    print(f"Cool i'm looking forward to {class2} too!")
+else:
+    print("Cool!")
 print(f"Well goodbye! I have to go now. It was nice meeting you {name}!")
 print("The End.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# hi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+secret = input("")
+if secret == "big secret code":
+    print("secret code!!!111!!111!11111")
